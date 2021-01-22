f6e7480cdd6a8683a0045acacc4dcb83c8cfa0fa
DanieleDeIuliis <daniele.deiuliis@gmail.com>
2021-01-22 16:04:14 +0100

    created a receipt builder

---
+++ src/main/kotlin/com/salestaxes/ReceiptBuilder.kt
@@ -0,0 +1,19 @@
+package com.salestaxes
+
+import java.lang.StringBuilder
+
+class ReceiptBuilder(
+    private val basket: Basket,
+    private val taxCalculator: TaxCalculator,
+    private val costCalculator: CostCalculator
+) {
+    fun buildReceipt(): String {
+        val stringBuilder = StringBuilder()
+        basket.items.forEach { (item, quantity) -> stringBuilder.append("$quantity ${item.name}: ${costCalculator.computePriceAfterTaxOf(item)}\n")}
+        stringBuilder
+            .append("Sales taxes: ${taxCalculator.computeTotalTaxes(basket.items)}\n")
+            .append("Total: ${costCalculator.computeTotalCost(basket.items)}")
+        return stringBuilder.toString()
+    }
+
+}
--- src/test/kotlin/com/salestaxes/ReceiptBuilderTest.kt
+++ src/test/kotlin/com/salestaxes/ReceiptBuilderTest.kt
@@ -1,39 +1,37 @@
 package com.salestaxes
 
+import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.lang.StringBuilder
 
 class ReceiptBuilderTest {
-//    val basket = mockk<Basket>()
-//
-//    @Test
-//    fun XXX() {
-//        val items = mapOf(
-//            Item("chocolate bar", 10.0, false, false) to 1,
-//            Item("chair", 30.0, false, true) to 2,
-//            Item("imported cigars", 40.0, true, true) to 1
-//        )
-//        val receiptBuilder = ReceiptBuilder()
-//        val receipt = receiptBuilder.buildReceiptFor(items)
-//        assertThat(receipt).isEqualToIgnoringCase("""
-//            |1 chocolate bar: 10.0
-//            |2 chair: 36.0
-//            |1 imported cigars: 46.0
-//            |Sales taxes: 12.0
-//            |Total: 92.0
-//        """.trimMargin())
-//
-//    }
-//}
-//
-//class ReceiptBuilder() {
-//    fun buildReceiptFor(items: Map<Item, Int>): String {
-//        val stringBuilder = StringBuilder()
-//        items.forEach {
-//            (item, quantity) -> stringBuilder.append()
-//        }
-//    }
 
+    private val taxCalculator: TaxCalculator = mockk()
+    private val costCalculator: CostCalculator = mockk()
+
+    @Test
+    fun `Build a receipt formatted properly`() {
+        every { taxCalculator.computeTotalTaxes(any()) } returns 12.0
+        every { taxCalculator.computeTaxAmount(any()) } returns 3.0
+        every { costCalculator.computeTotalCost(any()) } returns 92.0
+        every { costCalculator.computePriceAfterTaxOf(any()) } returns 20.0
+        val items = mapOf(
+            Item("chocolate bar", 10.0, false, false) to 1,
+            Item("chair", 30.0, false, true) to 2,
+            Item("imported cigars", 40.0, true, true) to 1
+        )
+        val basket = Basket(items)
+        val receiptBuilder = ReceiptBuilder(basket, taxCalculator, costCalculator)
+        val receipt = receiptBuilder.buildReceipt()
+        assertThat(receipt).isEqualToIgnoringCase("""
+            |1 chocolate bar: 20.0
+            |2 chair: 20.0
+            |1 imported cigars: 20.0
+            |Sales taxes: 12.0
+            |Total: 92.0
+        """.trimMargin())
+
+    }
 }
+
