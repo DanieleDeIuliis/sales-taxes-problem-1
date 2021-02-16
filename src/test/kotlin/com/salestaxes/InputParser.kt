0a38a1dc782379b2c8cbf134d0b9f2659aa4bff6
DanieleDeIuliis <daniele.deiuliis@gmail.com>
2021-02-16 18:03:21 +0100

    completed input parser

--- src/test/kotlin/com/salestaxes/InputParser.kt
+++ src/test/kotlin/com/salestaxes/InputParser.kt
@@ -9,9 +9,26 @@ class InputParser {
     private fun parseItemFrom(line: String): Item {
         val pattern = "(\\d+) (.*) at (\\d+\\.\\d{2})".toRegex()
         val resultGroups = pattern.find(line)?.groups
-        val quantity = resultGroups?.get(1)?.value ?: null
-        val price = resultGroups?.get(3)?.value ?: null
-        return Item("", true, getPriceOrThrow(price), getQuantityOrThrow(quantity))
+        val quantity = resultGroups?.get(1)?.value
+        val name = resultGroups?.get(2)?.value
+        val price = resultGroups?.get(3)?.value
+        return Item(getNameOrThrow(name), getIsImportedOrThrow(name), getPriceOrThrow(price), getQuantityOrThrow(quantity))
+    }
+
+    private fun getNameOrThrow(name: String?): String {
+        try {
+            return name!!.split(" ").filter { !it.equals("imported", ignoreCase = true) }.joinToString(" ")
+        } catch (e: Exception) {
+            throw InvalidInputException()
+        }
+    }
+
+    private fun getIsImportedOrThrow(name: String?): Boolean {
+        try {
+            return name!!.contains("imported", ignoreCase = true)
+        } catch (e: Exception) {
+            throw InvalidInputException()
+        }
     }
 
     private fun getQuantityOrThrow(quantity: String?): Int {
--- src/test/kotlin/com/salestaxes/InputParserTest.kt
+++ src/test/kotlin/com/salestaxes/InputParserTest.kt
@@ -1,5 +1,6 @@
 package com.salestaxes
 
+import org.assertj.core.api.Assertions.`in`
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -8,7 +9,7 @@ class InputParserTest {
 
 
     @Test
-    fun `returns a properly formatted item when a single line input is passed`() {
+    fun `returns the correct price when a single line input is passed`() {
         val input = "2 imported book at 12.49"
         val inputParser = InputParser()
 
@@ -79,14 +80,70 @@ class InputParserTest {
         assertThrows<InvalidInputException> { inputParser.parse(input) }
     }
 
-//    @Test
-//    fun `returns a properly formatted item when a single line input is passed`() {
-//        val input = "2 imported book at 12.49"
-//        val inputParser = InputParser()
-//
-//        val parsedItems = inputParser.parse(input)
-//
-//        assertThat(parsedItems).isNotEmpty
-//        assertThat(parsedItems.first()).isEqualTo(Item(name = "book", isImported = true, price = 12.49, quantity = 2 ))
-//    }
+    @Test
+    fun `returns the correct isImported when the input is valid`() {
+        val input = "2 imported book at 12.49"
+        val inputParser = InputParser()
+
+        val parsedItems = inputParser.parse(input)
+
+        assertThat(parsedItems).isNotEmpty
+        val item = Item(name = "book", isImported = true, price = 12.49, quantity = 2)
+        assertThat(parsedItems.first().isImported).isEqualTo(item.isImported)
+    }
+
+    @Test
+    fun `returns the correct isImported when the input is a not imported valid item`() {
+        val input = "2 book at 12.49"
+        val inputParser = InputParser()
+
+        val parsedItems = inputParser.parse(input)
+
+        assertThat(parsedItems).isNotEmpty
+        val item = Item(name = "book", isImported = false, price = 12.49, quantity = 2)
+        assertThat(parsedItems.first().isImported).isEqualTo(item.isImported)
+    }
+
+    @Test
+    fun `throws InvalidInputException when the input does not contain a name`() {
+        val input = "2 at 12.49"
+        val inputParser = InputParser()
+
+        assertThrows<InvalidInputException> { inputParser.parse(input) }
+    }
+
+    @Test
+    fun `returns the correct name when the input is valid`() {
+        val input = "2 imported book at 12.49"
+        val inputParser = InputParser()
+
+        val parsedItems = inputParser.parse(input)
+
+        assertThat(parsedItems).isNotEmpty
+        val item = Item(name = "book", isImported = true, price = 12.49, quantity = 2)
+        assertThat(parsedItems.first().name).isEqualTo(item.name)
+    }
+
+    @Test
+    fun `returns the correct name when the input is a valid not imported item`() {
+        val input = "2 wonderful book at 12.49"
+        val inputParser = InputParser()
+
+        val parsedItems = inputParser.parse(input)
+
+        assertThat(parsedItems).isNotEmpty
+        val item = Item(name = "wonderful book", isImported = true, price = 12.49, quantity = 2)
+        assertThat(parsedItems.first().name).isEqualTo(item.name)
+    }
+
+    @Test
+    fun `returns a properly formatted item when a single line input is passed`() {
+        val input = "2 imported book at 12.49"
+        val inputParser = InputParser()
+
+        val parsedItems = inputParser.parse(input)
+
+        assertThat(parsedItems).isNotEmpty
+        assertThat(parsedItems.first()).isEqualTo(Item(name = "book", isImported = true, price = 12.49, quantity = 2 ))
+    }
 }
